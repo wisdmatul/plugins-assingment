09f7c72c2fee7c2e939407a47bfb6a5b96be0f90
Atul <atul.kumar@wisdmlabs.com>
2023-03-16 01:23:39 +0530

    completed the form for plugin

--- content-calendar.php
+++ content-calendar.php
@@ -28,19 +28,79 @@ if( !defined( 'WPAC_PLUGIN_DIR' ) )
     define( 'WPAC_PLUGIN_DIR', plugin_dir_url(__FILE__) );
 }
 
-if( !function_exists( 'wpac_plugin_scripts' ) )
+if( !function_exists( 'wp_plugin_scripts' ) )
 {
     function wpac_plugin_scripts()
     {
         //to include frontend css
-        wp_enqueue_style('wpac-css', WPAC_PLUGIN_DIR . 'assets/css/style.css');
+        wp_enqueue_style('wp-css', WPAC_PLUGIN_DIR . 'assets/css/style.css');
 
         //to include javascript file
-        wp_enqueue_script('wpac-js', WPAC_PLUGIN_DIR . 'assets/js/main.js');
+        wp_enqueue_script('wp-js', WPAC_PLUGIN_DIR . 'assets/js/main.js');
 
     }
-    add_action( 'wp_enqueue_scripts', 'wpac_plugin_scripts' );
+    add_action( 'wp_enqueue_scripts', 'wp_plugin_scripts' );
 }
 
 //setting the menu option
 require plugin_dir_path(__FILE__).'include/setting.php';
+
+//adding form for calendar into the page
+function form_page()
+{
+    ?>
+    <div class = "form-container">
+        <form method="post" class = "content-form">
+        <div>
+            <label for="date">Date</label>
+            <input type="date" name="date" id="date" required><br>
+        </div>
+        <div>
+            <label for="occasion">Occassion</label>
+            <input type="text" name="occassion" id="occasion"><br>
+        </div>
+        <div>
+            <label for="post_title">Post Title</label>
+            <input type="text" name="post_title" id="post_title"><br>
+        </div>
+        <div>
+            <label for="author">Author</label>
+            <select name="author" id="author" rquired>
+                <?php
+                    $users =  get_users(array(
+                        'fields' =>array('ID', 'display_name')
+                    ));
+                    foreach($users as $user)
+                    {
+                        echo '<option value=">'. $user->ID. '">'. $user->display_name . '</option>';
+                    }
+                ?>
+            </select> <br>   
+        </div>
+        <div>
+            <label for="reviewer">Reviewer</label>
+            <select name="reviewer" id="reviewer" required>
+                <?php
+                    $admins =  get_users(array(
+                        'role' => 'administrator',
+                        'fields' =>array('ID', 'display_name')
+                    ));
+                    foreach($admins as $admin)
+                    {
+                        echo '<option value=">'. $admin->ID. '">'. $admin->display_name . '</option>';
+                    }
+                ?>
+            </select>
+        </div>
+        <div class="btn">
+            <?php submit_button('Schedule Post');  ?>
+        </div>
+        </form>
+    </div>
+    <?php
+}
+
+function calendar_callback()
+{
+    form_page();
+}
--- include/setting.php
+++ include/setting.php
@@ -1,16 +1,30 @@
 <?php
 
-function wpac_register_menu_page()
+function wp_register_menu_page()
 {
     add_menu_page(
         'WP Content calendar',
         'Content calendar',
          'manage_options',
-          'wpac-settings',
-           'wpac_setting_page_html',
+          'content-calendar',
+           'calendar_callback',
             'dashicons-calendar',
              5 );
     
 
 }
-add_action('admin_menu', 'wpac_register_menu_page');
+add_action('admin_menu', 'wp_register_menu_page');
+
+function wp_setting_page_html()
+{
+    if(!is_admin())
+    {
+        return ;
+    }
+
+    ?>
+        <div class = "wrap">
+            <h1><?= esc_html(get_admin_page_title()); ?></h1>
+        </div>
+    <?php
+}
