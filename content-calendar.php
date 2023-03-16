d89b1eeb0a31a25c2779d018c44442c802b21d8b
Atul <atul.kumar@wisdmlabs.com>
2023-03-16 13:54:15 +0530

    connected to database and created a table

--- content-calendar.php
+++ content-calendar.php
@@ -46,61 +46,101 @@ if( !function_exists( 'wp_plugin_scripts' ) )
 require plugin_dir_path(__FILE__).'include/setting.php';
 
 //adding form for calendar into the page
-function form_page()
-{
-    ?>
-    <div class = "form-container">
-        <form method="post" class = "content-form">
-        <div>
-            <label for="date">Date</label>
-            <input type="date" name="date" id="date" required><br>
-        </div>
-        <div>
-            <label for="occasion">Occassion</label>
-            <input type="text" name="occassion" id="occasion"><br>
-        </div>
-        <div>
-            <label for="post_title">Post Title</label>
-            <input type="text" name="post_title" id="post_title"><br>
-        </div>
-        <div>
-            <label for="author">Author</label>
-            <select name="author" id="author" rquired>
-                <?php
-                    $users =  get_users(array(
-                        'fields' =>array('ID', 'display_name')
-                    ));
-                    foreach($users as $user)
-                    {
-                        echo '<option value=">'. $user->ID. '">'. $user->display_name . '</option>';
-                    }
-                ?>
-            </select> <br>   
-        </div>
-        <div>
-            <label for="reviewer">Reviewer</label>
-            <select name="reviewer" id="reviewer" required>
-                <?php
-                    $admins =  get_users(array(
-                        'role' => 'administrator',
-                        'fields' =>array('ID', 'display_name')
-                    ));
-                    foreach($admins as $admin)
-                    {
-                        echo '<option value=">'. $admin->ID. '">'. $admin->display_name . '</option>';
-                    }
-                ?>
-            </select>
-        </div>
-        <div class="btn">
-            <?php submit_button('Schedule Post');  ?>
-        </div>
-        </form>
-    </div>
-    <?php
+require plugin_dir_path(__FILE__).'include/form.php';
+
+//connecting to the database and table creation
+function create_event_tables() {
+    global $wpdb;
+
+    $table_name = $wpdb->prefix . "events";
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name(
+        id mediumint(9) AUTO_INCREMENT,
+        date date NOT NULL,
+        occassion text,
+        post_title text NOT NULL,
+        author varchar(40) NOT NULL,
+        reviewer varchar(40) NOT NULL,
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
 }
 
-function calendar_callback()
+register_activation_hook( __FILE__, 'create_event_tables' );
+
+//form submission
+
+function form_submition()
 {
-    form_page();
+    global $wpdb;
+
+    if(isset($_POST['date']) && isset($_POST['occasion']) && isset($_POST['post_title']) && isset($_POST['author']) && isset($_POST['reviewer']))
+    {
+        $table_name = $wpdb->prefix . "events";
+        $date = sanitize_text_field( $_POST['date'] );
+        $occassion = sanitize_text_field( $_POST['occassion'] );
+        $post_title = sanitize_text_field( $_POST['post_title'] );
+        $author = sanitize_text_field( $_POST['author'] );
+        $reviewer = sanitize_text_field( $_POST['reviewer'] );
+
+        $wpdb->insert(
+            $table_name,
+            array(
+                'date'       => $date,
+                'occassion'  => $occassion,
+                'post_title' => $post_title,
+                'author'     => $author,
+                'reviewer'   => $reviewer
+            )
+        );
+    }
 }
+
+add_action('init', 'btn_submit');
+
+function btn_submit()
+{
+    if(isset( $_POST['submit'] ))
+    {
+        form_submition();
+    }
+}
+
+
+function schedule()
+{
+    ?>
+    <h1 class = "table-title">Upcoming Events</h1>
+    <?php
+
+    global $wpdb;
+
+    $table_name = $wpdb->prefix.'events';
+    $results = $wpdb->get_results("SELECT * FROM $table_name WHERE date >= DATE(NOW()) ORDER BY date");
+
+    echo '<table id="upcoming-table">';
+    echo '<thead><tr><th>ID</th><th>Date</th><th>Occasion</th><th>Post Title</th><th>Author</th><th>Reviewer</th></tr></thead>';
+
+    foreach($results as $row)
+    {
+        echo '<tr>';
+        echo '<td>'. $row->id . '</td>';
+        echo '<td>' . $row->date . '</td>';
+        echo '<td>' . $row->occasion . '</td>';
+        echo '<td>' . $row->post_title . '</td>';
+        echo '<td>' . get_userdata($row->author)->user_login . '</td>';
+        echo '<td>' . get_userdata($row->reviewer)->user_login . '</td>';
+        echo '</tr>';
+    }
+
+    echo '</table>';
+}
+
+?>
+
+<><>
+
+
